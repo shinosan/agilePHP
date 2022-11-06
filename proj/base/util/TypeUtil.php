cb5dc63283725526e0f9cf98c10c75d61f082800
shinosan <shinosan@iris.ocn.ne.jp>
2022-11-06 09:29:12 +0900

    比較演算子やデータ型列挙をenumに変更

--- proj/base/util/TypeUtil.php
+++ proj/base/util/TypeUtil.php
@@ -1,71 +1,140 @@
 <?php
+require_once __DIR__ . '/_dir.php';
+require_once BASE::UTIL . 'EnumUtil.php';
+
+/**
+ * データ型の列挙体
+ */
+enum Types {
+	case NULL;
+	case BOOL;
+	case INT;
+	case FLOAT;
+	case STRING;
+	case ARRAY;
+	case ENUM;
+	case DATETIME;
+	case OBJECT;
+	case MODEL;
+}
 
 /**
  * 型判定ユーティリティ
  */
-class TypeUtil {
+class TypeUtil extends EnumUtil {
+	/** インスタンス化禁止 */
 	private function __construct() {
 	}
 
-	/** @var string null型 */
-	const NULL = 'null';
-	/** @var string bool型 */
-	const BOOL = 'bool';
-	/** @var string int型 */
-	const INT = 'int';
-	/** @var string float型 */
-	const FLOAT = 'float';
-	/** @var string string型 */
-	const STRING = 'string';
-	/** @var string 配列型 */
-	const ARRAY = 'array';
-	/** @var string 日時型 */
-	const DATETIME = 'DateTime';
-	/** @var string オブジェクト型 */
-	const OBJECT = 'Object';
-	/** @var string フレームワークが管理するモデル */
-	const MODEL = 'Model';
-
 	/**
-	 * 値の型名を得て、 boolean=>bool, integer=>int, double=>float として返す
+	 * 値の型名を得て、 既知の型はTypes、未知の型は文字列 として返す
 	 * @param mixed $value 型名を知りたい値
 	 * @param bool $checkClass object クラス名を返す(省略=true)
-	 * @return string 型名
+	 * @return Types|string 型名
 	 */
-	public static function getType(mixed $value, bool $checkClass = true): string {
+	public static function getType(mixed $value, bool $checkClass = true): Types|string {
 		$type = gettype($value);
 		switch ($type) {
 			case 'boolean':
-				return self::BOOL;
+				return Types::BOOL;
 			case 'integer':
-				return self::INT;
+				return Types::INT;
 			case 'double':
-				return self::FLOAT;
+				return Types::FLOAT;
+			case 'array':
+				return Types::ARRAY;
+			case 'string':
+				return Types::STRING;
 			case 'object':
 				if ($value instanceof DateTime) {
-					return self::DATETIME;
+					return Types::DATETIME;
 				}
 				if ($value instanceof Model) {
-					return self::MODEL;
+					return Types::MODEL;
 				}
-				$type = $checkClass ? get_class($value) : self::OBJECT;
+				if (self::isEnum($value)) {
+					return Types::ENUM;
+				}
+				$type = $checkClass ? get_class($value) : Types::OBJECT;
 				break;
 		}
-		return $type;
+		return ($value === null) ? Types::NULL : $type;
 	}
-	public static function convertVal(string $type, ?string $val): mixed {
+	public static function convertVal(Types $type, ?string $val): mixed {
 		switch ($type) {
-			case TypeUtil::NULL:
+			case Types::NULL:
 				return null;
-			case TypeUtil::BOOL:
+			case Types::BOOL:
 				return is_bool($val) ? boolval($val) : null;
-			case TypeUtil::INT:
+			case Types::INT:
 				return is_int($val) ? intval($val) : null;
-			case TypeUtil::FLOAT:
+			case Types::FLOAT:
 				return is_float($val) ? floatval($val) : null;
-			case TypeUtil::DATETIME:
+			case Types::STRING:
+				return is_string($val) ? $val : null;
+			case Types::ARRAY:
+				return is_array($val) ? explode(',', $val) : null;
+			case Types::ENUM:
+				return self::toEnum($val);
+			case Types::OBJECT:
+				return is_object($val) ? $val : null;
+			case Types::DATETIME:
 				return DateUtil::isDateTime($val) ? DateUtil::toDateTime($val) : null;
+			case Types::MODEL:
+				return ($val instanceof Model) ? $val : null;
 		}
 		return null;
 	}
+
+	public static function isEnum(mixed $val): bool {
+		try {
+			$class = new ReflectionClass($val);
+			return $class->isEnum();
+		} catch (Exception) {
+			return false;
+		}
+	}
+	public static function toEnum(string $val): mixed {
+		list($enum, $case) = explode('::', $val);
+		foreach ($enum::cases() as $enum) {
+			if ($enum->name == $case) return $enum;
+		}
+		return null;
+	}
+
+	//// EnumUtil メソッド ////
+
+	/**
+	 * ID or 名称でデータ型を得る
+	 * @param int|string $key データ型のID or 名称
+	 * @return Types データ型
+	 */
+	public static function get(int|string $key): Types {
+		return self::getBase(self::$byIdList, self::$byNameList, $key);
+	}
+	/**
+	 * データ型のIDを得る
+	 * @param Types $enum データ型
+	 * @return int ID
+	 */
+	public static function id(Types $enum): int {
+		return self::idBase(self::$byNameList, $enum);
+	}
+	/**
+	 * データ型の一覧を返す
+	 * @return array Typesの一覧
+	 */
+	public static function getEnums(): array {
+		return self::$byIdList;
+	}
+	/**
+	 * データ型の列挙体を配列に登録する
+	 * @params Types ...$enums 登録する列挙体
+	 */
+	public static function register(Types ...$enums) {
+		self::registerBase(self::$byIdList, self::$byNameList, ...$enums);
+	}
+	private static array $byIdList = [];
+	private static array $byNameList = [];
 }
+TypeUtil::register(Types::NULL, Types::BOOL, Types::INT, Types::FLOAT, Types::STRING, Types::ARRAY, Types::DATETIME, Types::OBJECT, Types::MODEL);
