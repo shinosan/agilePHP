e4da790dedfa3a853feac8125c94a14bdcdebfba
shinosan <shinosan@iris.ocn.ne.jp>
2022-11-20 23:16:03 +0900

    クライアントとの送受信を修正。

--- proj/apps/game/logic/GameServer.php
+++ proj/apps/game/logic/GameServer.php
@@ -6,6 +6,9 @@ require_once \GAME::MODEL . 'Chara.php';
 use Ratchet\MessageComponentInterface;
 use Ratchet\ConnectionInterface;
 
+/**
+ * ゲームの送受信サーバ
+ */
 class GameServer implements MessageComponentInterface {
 
 	protected $clients;
@@ -18,16 +21,28 @@ class GameServer implements MessageComponentInterface {
 		$this->clients->attach($conn);
 	}
 
-	public function onMessage(ConnectionInterface $from, $msg) {
+	// protected float $sendDuration = 0.01; // 0.1秒に1回
+	protected float $sendDuration = 3; // DEBUG: ３秒に1回
+	protected float $prevSendTime = 0;
+
+	/**
+	 * 各プレイヤーからの入力データを受けつけ、処理する  
+	 * 送信間隔時間が過ぎたら、全キャラの更新されたデータを配信する
+	 */
+	public function onMessage(ConnectionInterface $from, mixed $input) {
+		// 入力データを処理
+		$data = $this->process($input);
+
+		// 送信間隔時間に達していなければ送信しない
+		$curTime = microtime(true);
+		if ($curTime - $this->prevSendTime < $this->sendDuration) return;
+
+		// 全クライアントに配信
 		foreach ($this->clients as $client) {
-			$data = ['msg' => $msg];
-			if ($from === $client) {
-				$data['position'] = 'right';
-			} else {
-				$data['position'] = 'left';
-			}
+			$data['position'] = ($from === $client) ? 'right' : 'left';
 			$client->send(json_encode($data));
 		}
+		$this->prevSendTime = $curTime;
 	}
 
 	public function onClose(ConnectionInterface $conn) {
@@ -36,5 +51,14 @@ class GameServer implements MessageComponentInterface {
 
 	public function onError(ConnectionInterface $conn, \Exception $e) {
 		$conn->close();
+	}
+
+	/**
+	 * データを処理する
+	 */
+	public function process(string $msg): ?array {
+		// TODO: 本実装する
+		$data = ['msg' => $msg];
+		return $data;
 	}
 }
